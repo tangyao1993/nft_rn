8ed7a9fa42715d7675cc7c5a5fc7586d8b03296b
ty <345575577@qq.com>
2022-07-17 15:25:55 +0800

    完成市场页面

--- Readme.md
+++ Readme.md
@@ -12,3 +12,54 @@ reject(err.data)
 })    
 });}
 
+
+
+
+
+axios.get("", {params: {}}).then(
+response => {
+const resp = response.data;
+
+                if (resp.code === 200) {
+                    this.setState({
+                        pics: this.state.dataArr.concat(resp.data.records),
+                    })
+                }
+
+            }).catch(error => {
+            console.log(error);
+        })
+
+
+
+1、Content-Type: application/json
+import axios from 'axios'
+let data = {"code":"1234","name":"yyyy"};
+axios.post(`${this.$url}/test/testRequest`,data)
+.then(res=>{
+console.log('res=>',res);            
+})
+
+
+
+2、Content-Type: multipart/form-data
+import axios from 'axios'
+let data = new FormData();
+data.append('code','1234');
+data.append('name','yyyy');
+axios.post(`${this.$url}/test/testRequest`,data)
+.then(res=>{
+console.log('res=>',res);            
+})
+
+
+3、Content-Type: application/x-www-form-urlencoded
+import axios from 'axios'
+import qs from 'Qs'
+let data = {"code":"1234","name":"yyyy"};
+axios.post(`${this.$url}/test/testRequest`,qs.stringify({
+data
+}))
+.then(res=>{
+console.log('res=>',res);            
+})
--- app/market/Market.js
+++ app/market/Market.js
@@ -1,15 +1,182 @@
 import React, {Component} from 'react';
-import {Text, View} from "react-native";
+import {Text, View, StyleSheet, Image, FlatList, ScrollView, ActivityIndicator} from "react-native";
+import Swiper from 'react-native-swiper'
+import axios from "axios";
+import {Flex, WingBlank} from "@ant-design/react-native";
+
+const styles = StyleSheet.create({
+    wrapper: {flex:1},
+    mainImgCss:{
+        width:420,
+        height:200
+    },
+    imgCss:{
+        width:150,
+        height:150
+    },
+    wingBlankCss:{
+        marginTop: 5,
+        marginBottom: 5,
+        marginLeft:30
+    },
+    swiperCss:{
+        height:200
+    }
+})
+
 
 class Market extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pics: [],
+            picPageNum:1,
+            picPageSize:5,
+            pageNum:1,
+            pageSize:10,
+            totalPage:0,
+            isLoading:false,
+            dataArr:[],
+        }
+        this.getPics();
+        this.getData();
+    }
+
+    getPics = () => {
+        console.log("getPics")
+        axios.get("/super/market/list", {params: {current: this.state.picPageNum, size: this.state.picPageSize}}).then(
+            response => {
+                const resp = response.data;
+
+                if (resp.code === 200) {
+                    this.setState({
+                        pics: resp.data.records,
+                    })
+                }
+
+            }).catch(error => {
+            console.log(error);
+        })
+
+    }
+
+    getData = () => {
+        console.log("getData")
+        axios.get("/market/group",{params:{current:this.state.pageNum,size:this.state.pageSize}}).then(
+            response=>{
+                const resp = response.data;
+
+                //console.log(resp);
+                if (resp.code === 200){
+                    this.setState({
+                        dataArr:this.state.dataArr.concat(resp.data.records),
+                        totalPage:resp.data.pages
+                    })
+                }
+
+            }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    /*上拉加载*/
+    loadMoreData = () =>{
+        if (this.state.pageNum +1 >this.state.totalPage){
+            return;
+        }
+
+        this.setState({
+            pageNum:this.state.pageNum + 1,
+            pageSize:this.state.pageSize,
+        },()=>{
+            this.getData();
+        })
+    }
+
+    /*下拉刷新*/
+    loadRefreshData = () => {
+        //1、修改 isLoading的值为true
+        this.setState({
+            isLoading:true
+        })
+
+        //2、异步请求
+        //清理array
+        this.setState({
+            dataArr:[],
+            pageNum:1,
+            pageSize:this.state.pageSize,
+        },()=>{
+            //获取初始化数据
+            this.getPics();
+            this.getData();
+        })
+        this.setState({
+            isLoading:false,
+        })
+    }
+
+    /*渲染数据*/
+    renderData({item}){
+        return(
+            <WingBlank style={styles.wingBlankCss}>
+                <Flex direction={"column"}>
+                    <Flex.Item>
+                        <Image style={styles.imgCss} source={require("../resource/img.png")}/>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <Text>名称:{item.name}</Text>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <Text>作者:{item.genUserName}</Text>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <Text>价格:{item.price}</Text>
+                    </Flex.Item>
+                </Flex>
+            </WingBlank>
+        )
+    }
+
+    renderSwiper = () =>{
+        return <Swiper style={styles.swiperCss}>
+            {
+                this.state.pics.map((item,i)=>{
+                    return(
+                        <View>
+                            <Image style={styles.mainImgCss} source={require("../resource/img.png")}/>
+                        </View>
+                    )
+                })
+            }
+        </Swiper>
+    }
+
     render() {
+        if (this.state.isLoading){
+            return (
+                <ActivityIndicator
+                    size="small"
+                    animating={true}
+                    color="#ccc"
+                />
+            )
+        }
         return (
-            <View>
-                <Text>
-                    这是市场
-                </Text>
-            </View>
-        );
+            <FlatList
+                ListHeaderComponent={this.renderSwiper}
+                numColumns ={2}
+                data = {this.state.dataArr}
+                keyExtractor={(item,i)=>i}
+                renderItem = {this.renderData}
+                onEndReached = {this.loadMoreData}
+                onEndReachedThreshold={0.5}
+                refreshing = {this.state.isLoading}  //设置是否正在加载数据
+                onRefresh = {this.loadRefreshData} //设置刷新的时候，执行的代码
+            />
+
+        )
     }
 }
 
