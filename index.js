317dcee7b42e5c8fce91c509606921c9ad780512
yao.tang <345575577@qq.com>
2022-08-10 19:37:37 +0800

    修改请求地址

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ import { registerRootComponent } from 'expo';
 import App from './app/App';
 import axios from "axios";
 
-axios.defaults.baseURL='http://106.12.117.221:8080';
+axios.defaults.baseURL='http://106.12.117.221:38080';
 registerRootComponent(App);
